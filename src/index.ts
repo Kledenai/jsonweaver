5ecbe497d0a0c3234af7a1f36bfba3873ac13cde
kledenai <bruno@khaneland.com>
2025-01-03 06:17:07 -0300

    adding index.tsx

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { toCSV } from './toCSV';
+import { toXML } from './toXML';
+import { toMarkdownTable } from './toMarkdownTable';
+
+export const jsonweaver = {
+  toCSV,
+  toXML,
+  toMarkdownTable,
+};
