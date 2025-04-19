7fbbb1e0070fdd7afa1fd0266d045f956e8af5b0
Bruno Rocha <bruno.rocha@beetech.com.br>
2025-04-19 19:51:16 -0300

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
-module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+export default {
+  transform: {},
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {},
+  testEnvironment: 'node',
+  preset: 'ts-jest/presets/default-esm',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
 };
