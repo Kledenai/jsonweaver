fdeb196c59709361fbad254ea51354789b3de3fc
Bruno Rocha <bruno.rocha@beetech.com.br>
2025-04-27 18:21:26 -0300

    Add new features to export

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import { toJsonLines, toJsonLinesStream } from './converters/toJsonLines';
-import { toMarkdownTable } from './converters/toMarkdownTable';
-import { toYaml } from './converters/toYaml';
+import validateJsonSchema from './utils/helpers/validateJsonSchema';
+import toMarkdownTable from './converters/toMarkdownTable';
+import batchProcess from './utils/helpers/batchProcess';
 import { toCSV } from './converters/toCSV';
-import { toXML } from './converters/toXML';
+import toYaml from './converters/toYaml';
+import toXML from './converters/toXML';
 
 export const jsonweaver = {
+  validateJsonSchema,
   toJsonLinesStream,
   toMarkdownTable,
+  batchProcess,
   toJsonLines,
   toYaml,
   toCSV,
